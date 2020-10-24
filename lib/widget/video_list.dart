1d5e8ac1a2d2070cd549b3a9c3de8aaa2c14b4e7
hj <dv.zinke@gmail.com>
2020-10-24 23:31:24 +0900

    feature(widget): 비디오 리스트 추가

---
+++ lib/widget/video_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VideoList extends StatefulWidget {
+  @override
+  _VideoListState createState() => _VideoListState();
+}
+
+class _VideoListState extends State<VideoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
