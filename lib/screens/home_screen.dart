976d535ff2f070482fdba5e2e323975729ad3794
hj <dv.zinke@gmail.com>
2020-10-26 23:55:21 +0900

    feature(widget): 이미지위에 시간 표시할 위젯 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:kpop_youtube/models/play_list_item.dart';
-import 'package:kpop_youtube/screens/video_play.dart';
 import 'package:kpop_youtube/screens/video_play2.dart';
-import 'package:kpop_youtube/screens/video_player_screen.dart';
 import 'package:kpop_youtube/utils/services.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -40,9 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             DrawerHeader(
               child: Text("Drawer Header"),
-              decoration: BoxDecoration(
-                color: Colors.red
-              ),
+              decoration: BoxDecoration(color: Colors.red),
             ),
             ListTile(
               title: Text("dd 1"),
@@ -53,63 +49,84 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text("KPOPTUBE"),
       ),
-      body: _isLoading ? CircularProgressIndicator() : Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Expanded(
-              child: NotificationListener<ScrollEndNotification>(
-                onNotification: (ScrollNotification notification){
-                  if(notification.metrics.pixels == notification.metrics.maxScrollExtent) {
-                    print("끝");
-                  }
-                  return true;
-                },
-                child: ListView.builder(
-                  controller: _scrollController,
-                  itemCount: _playlistItem.items.length,
-                  itemBuilder: (context, index) {
-                    Item item = _playlistItem.items[index];
-                    return InkWell(
-                      onTap: () async {
-                        print("https://youtube.com/embed/${item.snippet.resourceId.videoId}?autoplay=1");
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                          return VideoPlay2(videoId: item.snippet.resourceId.videoId,);
-                        }));
+      body: _isLoading
+          ? CircularProgressIndicator()
+          : Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: NotificationListener<ScrollEndNotification>(
+                      onNotification: (ScrollNotification notification) {
+                        if (notification.metrics.pixels ==
+                            notification.metrics.maxScrollExtent) {
+                          print("끝");
+                        }
+                        return true;
                       },
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Row(
-                          children: [
-                            DecoratedBox(
-                              decoration: BoxDecoration(color: Colors.black),
-                              child: IndexedStack(
+                      child: ListView.builder(
+                        controller: _scrollController,
+                        itemCount: _playlistItem.items.length,
+                        itemBuilder: (context, index) {
+                          Item item = _playlistItem.items[index];
+                          return InkWell(
+                            onTap: () async {
+                              print(
+                                  "https://youtube.com/embed/${item.snippet.resourceId.videoId}?autoplay=1");
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return VideoPlay2(
+                                  videoId: item.snippet.resourceId.videoId,
+                                );
+                              }));
+                            },
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              child: Row(
                                 children: [
-                                  CachedNetworkImage(
-                                    imageUrl: item.snippet.thumbnails.thumbnailsDefault.url,
-                                    placeholder: (context, url) => CircularProgressIndicator(),
-                                    errorWidget: (context, url, error) => Icon(Icons.error),
+                                  Stack(
+                                    children: [
+                                      CachedNetworkImage(
+                                        imageUrl: item.snippet.thumbnails
+                                            .thumbnailsDefault.url,
+                                        placeholder: (context, url) =>
+                                            CircularProgressIndicator(),
+                                        errorWidget: (context, url, error) =>
+                                            Icon(Icons.error),
+                                      ),
+                                      Positioned(
+                                        child: Container(
+                                          child: Text(
+                                            "test",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          decoration: BoxDecoration(
+                                              color: Colors.black),
+                                        ),
+                                        bottom: 0,
+                                        right: 0,
+                                      )
+                                    ],
                                   ),
-                                  Positioned(child:
-                                  Text("A", style: TextStyle(color: Colors.white),), bottom: 0, right: 0,)
-
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Flexible(child: Text(item.snippet.title)),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
                                 ],
                               ),
                             ),
-                            SizedBox(width: 20,),
-                            Flexible(child: Text(item.snippet.title)),
-                            SizedBox(width: 20,),
-                          ],
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
     );
   }
 }
