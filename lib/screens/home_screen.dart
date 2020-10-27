9f8e85b34f814363d4af6cc6f775fb0356848fc2
hj <dv.zinke@gmail.com>
2020-10-27 23:35:48 +0900

    feature(widget): 메뉴 변경 및 리스트 위젯 불러오게 변경

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:kpop_youtube/models/play_list_item.dart';
+import 'package:kpop_youtube/screens/YoutubeList1.dart';
 import 'package:kpop_youtube/screens/video_play2.dart';
 import 'package:kpop_youtube/utils/services.dart';
 
@@ -37,11 +38,14 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
-              child: Text("Drawer Header"),
+              child: Center(child: Text("KPOP TUBE", style: TextStyle(color: Colors.white, fontSize: 26),)),
               decoration: BoxDecoration(color: Colors.red),
             ),
             ListTile(
               title: Text("dd 1"),
+              onTap: ()=>{
+
+              },
             )
           ],
         ),
@@ -49,84 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text("KPOPTUBE"),
       ),
-      body: _isLoading
-          ? CircularProgressIndicator()
-          : Container(
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: NotificationListener<ScrollEndNotification>(
-                      onNotification: (ScrollNotification notification) {
-                        if (notification.metrics.pixels ==
-                            notification.metrics.maxScrollExtent) {
-                          print("끝");
-                        }
-                        return true;
-                      },
-                      child: ListView.builder(
-                        controller: _scrollController,
-                        itemCount: _playlistItem.items.length,
-                        itemBuilder: (context, index) {
-                          Item item = _playlistItem.items[index];
-                          return InkWell(
-                            onTap: () async {
-                              print(
-                                  "https://youtube.com/embed/${item.snippet.resourceId.videoId}?autoplay=1");
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return VideoPlay2(
-                                  videoId: item.snippet.resourceId.videoId,
-                                );
-                              }));
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: Row(
-                                children: [
-                                  Stack(
-                                    children: [
-                                      CachedNetworkImage(
-                                        imageUrl: item.snippet.thumbnails
-                                            .thumbnailsDefault.url,
-                                        placeholder: (context, url) =>
-                                            CircularProgressIndicator(),
-                                        errorWidget: (context, url, error) =>
-                                            Icon(Icons.error),
-                                      ),
-                                      Positioned(
-                                        child: Container(
-                                          child: Text(
-                                            "test",
-                                            style:
-                                                TextStyle(color: Colors.white),
-                                          ),
-                                          decoration: BoxDecoration(
-                                              color: Colors.black),
-                                        ),
-                                        bottom: 0,
-                                        right: 0,
-                                      )
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Flexible(child: Text(item.snippet.title)),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+      body:YoutubeList1()
     );
   }
 }
